5c94380354388c8863161f48b35409f8d54444b1
TimGank <tima.gankin@mail.ru>
2025-04-21 17:12:19 +0300

    2.1

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ YANDEX_API_KEY = "ваш_ключ"
 KUDAGO_API_KEY = "ваш_ключ"
 DATABASE_URL = "sqlite:///database/events.db"
 TELEGRAM_TOKEN = "7745071543:AAG75_9GIos3MDmDM1luGRSFFPlQJ9lsPhc"
+version = 1.4
