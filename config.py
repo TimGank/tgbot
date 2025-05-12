7dcf060f5836938c5b5cd96fe395831d60f576c4
TimGank <tima.gankin@mail.ru>
2025-05-12 21:24:41 +0300

    5.0(FULL DONE with documentation)

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
-# Ключи API и настройки
+# Настройки API
 KUDAGO_API_URL = "https://kudago.com/public-api/v1.4"
 TELEGRAM_TOKEN = "7745071543:AAG75_9GIos3MDmDM1luGRSFFPlQJ9lsPhc"
+
+# Настройки приложения
+MAX_EVENTS = 10  # Максимальное количество событий для показа
+REQUEST_TIMEOUT = 10  # Таймаут запросов к API (секунды)
